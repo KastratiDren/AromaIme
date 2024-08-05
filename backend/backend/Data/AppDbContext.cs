e3e9239d8243214637a7d1f085e9c8cdfda5a9b3
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-05 17:36:22 +0200

    Creating the fragrance model.

--- backend/backend/Data/AppDbContext.cs
+++ backend/backend/Data/AppDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data
 {
     public class AppDbContext :DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //Database Tabels
+        public DbSet<Fragrance> Fragrances { get; set; }
     }
 }
---
+++ backend/backend/Models/Fragrance.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    public class Fragrance
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(35, MinimumLength = 3 , ErrorMessage = "Name must be between 3 and 35 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(500, MinimumLength = 300, ErrorMessage = "Description must be between 300 and 500 characters.")]
+        public string Description { get; set; } =string.Empty;
+
+        [Required]
+        [Range(2.50, 1000.00, ErrorMessage = "Price must be between 2.50 and 1000.00.")]
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+    }
+}
