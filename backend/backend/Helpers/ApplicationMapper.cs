8b4737b0ad82b6b1dafa7e8fd081f64acda6b73e
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-06 09:01:01 +0200

    Configuring automapper.

---
+++ backend/backend/Helpers/ApplicationMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using backend.DTOs;
+using backend.Models;
+
+namespace backend.Helpers
+{
+    public class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            CreateMap<Fragrance, FragranceDto>().ReverseMap();
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Brand, BrandDTO>().ReverseMap();
+            CreateMap<Gender, GenderDTO>().ReverseMap();
+            CreateMap<Sillage, SillageDTO>().ReverseMap();
+            CreateMap<Longevity, LongevityDTO>().ReverseMap();
+            CreateMap<Scent, ScentDTO>().ReverseMap();
+            CreateMap<Season, SeasonDTO>().ReverseMap();
+        }
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
