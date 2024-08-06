ef72edeeda4b4629ed4a5e8b0d293478417ff3b6
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-06 16:44:27 +0200

    Creating the Category Controller.

---
+++ backend/backend/Controllers/CategoryController.cs
@@ -0,0 +1,85 @@
+﻿using backend.DTOs;
+using backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CategoryService _categoryService;
+        public CategoryController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categorys = await _categoryService.GetAllAsync();
+            return Ok(categorys);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var category = await _categoryService.GetAsync(id);
+            if(category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CategoryDTO categoryDTO)
+        {
+            if(categoryDTO == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdDTO = await _categoryService.CreateAsync(categoryDTO);
+            return Ok(createdDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if(id == 0)
+                return BadRequest();
+
+            var category = await _categoryService.DeleteAsync(id);
+            if (category == null)
+                return NotFound();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] CategoryDTO categoryDTO)
+        {
+            if (id == 0 || categoryDTO == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var updatedCategory = await _categoryService.UpdateAsync(id, categoryDTO);
+            if (updatedCategory == null)
+                return NotFound();
+
+            return Ok(updatedCategory);
+        }
+
+
+    }
+}
