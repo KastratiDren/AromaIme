6bb8ad9a350b9b17fd44dce955377e0cc50cb5f8
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-06 16:58:37 +0200

    Creating the scent controller.

---
+++ backend/backend/Controllers/ScentController.cs
@@ -0,0 +1,83 @@
+﻿using backend.DTOs;
+using backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScentController : ControllerBase
+    {
+        private readonly ScentService _scentService;
+        public ScentController(ScentService scentService)
+        {
+            _scentService = scentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var scents = await _scentService.GetAllAsync();
+            return Ok(scents);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var scent = await _scentService.GetAsync(id);
+            if (scent == null)
+                return NotFound();
+
+            return Ok(scent);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ScentDTO scentDTO)
+        {
+            if (scentDTO == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdDTO = await _scentService.CreateAsync(scentDTO);
+            return Ok(createdDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var scent = await _scentService.DeleteAsync(id);
+            if (scent == null)
+                return NotFound();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] ScentDTO scentDTO)
+        {
+            if (id == 0 || scentDTO == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var updatedScent = await _scentService.UpdateAsync(id, scentDTO);
+            if (updatedScent == null)
+                return NotFound();
+
+            return Ok(updatedScent);
+        }
+    }
+}
