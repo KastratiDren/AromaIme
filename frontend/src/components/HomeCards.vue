f0a92ff259b68cb133bf5be48e574c526c172df3
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-10 09:34:08 +0200

    Creating the card component.

--- frontend/src/components/HomeCards.vue
+++ frontend/src/components/HomeCards.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import Card from '@/components/Card.vue'
 </script>
 
 <template>
@@ -7,26 +7,26 @@
         <div class="container-xl lg:container m-auto">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
                 <!-- Customer Card -->
-                <div class="bg-lightPrimary p-6 rounded-lg shadow-md">
+                <Card>
                     <h2 class="text-2xl font-bold text-primary">For Customers</h2>
-                    <p class="mt-2 mb-4 text-gray-700">
+                    <p class="mt-2 mb-4 text-black-700">
                         Browse our Fragrances and find the right one today
                     </p>
                     <a href="fragrance.html" class="inline-block bg-primary text-white rounded-lg px-4 py-2 hover:bg-hover">
                         Browse Fragrances
                     </a>
-                </div>
+                </Card>
                 <!-- Companies Card -->
-                <div class="bg-lightSecondary p-6 rounded-lg shadow-md">
+                <Card bg="bg-lightSecondary">
                     <h2 class="text-2xl font-bold text-primary">For Companies</h2>
-                    <p class="mt-2 mb-4 text-gray-700">
-                        List your fragrance your favorite fragrance
+                    <p class="mt-2 mb-4 text-black-700">
+                        List your favorite fragrance to wear
                     </p>
                     <a href="add-fragrance.html"
                         class="inline-block bg-primary text-white rounded-lg px-4 py-2 hover:bg-hover">
                         Add Fragrance
                     </a>
-                </div>
+                </Card>
             </div>
         </div>
     </section>
