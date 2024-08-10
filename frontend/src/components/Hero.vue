d56c5590be209dc2242ae605e6d7136e52e80e9c
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-10 08:59:00 +0200

    Creating the vue hero component.

---
+++ frontend/src/components/Hero.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="bg-primary py-16 mb-4">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+            <div class="text-center">
+                <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+                    Aroma Ime
+                </h1>
+                <p class="my-4 text-xl text-white">
+                    Find the Fragrance that fits your style
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
-    <nav class="bg-primary border-b border-gray-800">
+    <nav class="bg-primary border-b border-white-600">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="flex h-20 items-center justify-between">
                 <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                     <!-- <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
                     <img class="h-10 w-auto" src="images/logo.png" alt="Vue Jobs" />
                     <span class="hidden md:block text-white text-2xl font-bold ml-2"
-                    >Vue Jobs</span
-                    > -->
+                    >Vue Jobs</span>
+                     -->
                     <div class="md:ml-auto">
                         <div class="flex space-x-2">
                             <a href="index.html"
@@ -15,7 +15,8 @@
                             <a href="fragrance.html"
                                 class="text-white hover:bg-secondary hover:text-white rounded-md px-3 py-2">Fragrances</a>
                             <a href="add-fragrance.html"
-                                class="text-white hover:bg-secondary hover:text-white rounded-md px-3 py-2">Add Fragance</a>
+                                class="text-white hover:bg-secondary hover:text-white rounded-md px-3 py-2">Add
+                                Fragance</a>
                         </div>
                     </div>
                 </div>
