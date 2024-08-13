97b09a0cd89e38fe402972fef5096e1efac9d60f
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-13 10:27:40 +0200

    Created the user model.

---
+++ backend/backend/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [MinLength(2)]
+        public string Surname { get; set; } = string.Empty;
+
+    }
+}
