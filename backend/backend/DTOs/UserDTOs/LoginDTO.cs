3f5264a0f0e3071a3edc30d5bf2cd83d9e04c2ab
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-14 13:09:41 +0200

    Created the LoginDTO.

---
+++ backend/backend/DTOs/UserDTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs.UserDTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        [MinLength(3)]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
