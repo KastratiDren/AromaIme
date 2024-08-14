26edc1580dc519b68a8ac3b8301e4f9ed6edfd6b
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-14 13:05:04 +0200

    Creating UserDTO which contains the JWT token.

---
+++ backend/backend/DTOs/UserDTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.DTOs.UserDTOs
+{
+    public class UserDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
