b1998be0852886edbb42809efd572de1a3f15e4c
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-14 11:43:57 +0200

    Creating the register UserDTO.

---
+++ backend/backend/DTOs/UserDTOs/RegisterDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs.UserDTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [MinLength(2)]
+        public string Surname { get; set; } = string.Empty;
+        [Required]
+        [MinLength(3)]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
