3e810d02d9c711f1bb562d13654c467ec0d5b726
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-16 12:06:19 +0200

    FIX:: Adding the isLoggedin state to our authStore.

--- frontend/src/useAuth.js
+++ frontend/src/useAuth.js
@@ -1,21 +1,29 @@
+// src/useAuth.js
+import { reactive, computed } from 'vue';
+
+const state = reactive({
+  isLoggedIn: !!localStorage.getItem('token'),
+  username: localStorage.getItem('userName') || ''
+});
+
 export function useAuth() {
-    const login = (token, userName) => {
-        state.isLoggedIn = true;
-        state.username = userName;
-        localStorage.setItem('token', token);
-        localStorage.setItem('userName', userName);
-    };
+  const login = (token, userName) => {
+    state.isLoggedIn = true;
+    state.username = userName;
+    localStorage.setItem('token', token);
+    localStorage.setItem('userName', userName);
+  };
 
-    const logout = () => {
-        state.isLoggedIn = false;
-        state.username = '';
-        localStorage.removeItem('token');
-        localStorage.removeItem('userName');
-    };
+  const logout = () => {
+    state.isLoggedIn = false;
+    state.username = '';
+    localStorage.removeItem('token');
+    localStorage.removeItem('userName');
+  };
 
-    return {
-        state,
-        login,
-        logout
-    };
+  return {
+    state: computed(() => state),
+    login,
+    logout
+  };
 }
