56dde923eede895414ead708031821ee5656d533
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-27 12:20:53 +0200

    Created the Cart Item Model.

---
+++ backend/backend/Models/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        //public int CartId { get; set; } 
+        //[ForeignKey("CartId")]
+        //public Cart Cart { get; set; } = null!;
+
+        public int FragranceId { get; set; }
+        [ForeignKey("FragranceId")]
+        public Fragrance Fragrance { get; set; } = null!;
+
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
