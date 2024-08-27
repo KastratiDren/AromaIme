b229c757334b2ce123143c8af6eeff1b04df9b27
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-27 13:11:47 +0200

    Created the OrderItemDTO.

---
+++ backend/backend/DTOs/OrderItemDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs
+{
+    public class OrderItemDTO
+    {
+        [Required]
+        public int FragranceId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "Quantity must be between 1 and 10.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+
+        public decimal TotalAmount => Quantity * Price;
+    }
+}
