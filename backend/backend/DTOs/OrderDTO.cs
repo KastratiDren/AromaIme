a99462bbc653b3202cf2d45531bdda5a4bc5deb0
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-27 13:11:33 +0200

    Created the OrderDTO.

---
+++ backend/backend/DTOs/OrderDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs
+{
+    public class OrderDTO
+    {
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string State { get; set; } = string.Empty;
+
+        [Required]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        public string StripePaymentIntentId { get; set; } = string.Empty;
+
+        [Required]
+        public string SessionId { get; set; } = string.Empty;
+
+        public List<OrderItemDTO> OrderItems { get; set; } = new List<OrderItemDTO>();
+
+        [Required]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal TotalAmount { get; set; }
+    }
+}
