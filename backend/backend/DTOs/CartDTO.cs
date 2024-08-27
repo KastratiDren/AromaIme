d309a738651410704b2b1b76f4e4f34f40480da0
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-27 13:11:11 +0200

    Created the CartDTO.

---
+++ backend/backend/DTOs/CartDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs
+{
+    public class CartDTO
+    {
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        // List of cart items to be displayed in the cart page
+        public List<CartItemDTO> CartItems { get; set; } = new List<CartItemDTO>();
+
+        [Required]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal TotalAmount { get; set; }
+    }
+}
