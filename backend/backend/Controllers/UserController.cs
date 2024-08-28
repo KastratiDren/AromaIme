15d84ca9b4a473313afaf5a1709c4b608de69f4a
Dren Kastrati <drenkastrati33@gmail.com>
2024-08-28 10:07:19 +0200

    FIX:: Got the roles in the User Service.

---
+++ backend/backend/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAllAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return BadRequest("Id can't be null or empty.");
+
+            var user = await _userService.GetAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return BadRequest("Id can't be null or empty.");
+
+            var result = await _userService.DeleteAsync(id);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
